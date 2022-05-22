30ba713effafd0780f50ab649c581e4744149fa6
jevon <jevon.levin@gamil.com>
2022-05-22 11:56:47 +0700

    fix typing animation

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import { memo, useEffect, useState } from 'react';
 import { VscLocation } from 'react-icons/vsc';
 import Clock from 'react-live-clock';
 import { Parallax } from 'react-scroll-parallax';
@@ -17,6 +17,24 @@ const Header = () => {
     });
   }, []);
 
+  // eslint-disable-next-line react/display-name
+  const TypingAnimation = memo(
+    () => {
+      return (
+        <Typical
+          loop={Infinity}
+          steps={[
+            'get updated weather from places around the world🌏',
+            2000,
+            'get updated weather in realtime⏳',
+            1000,
+          ]}
+        />
+      );
+    },
+    () => true
+  );
+
   const { data: weather, isValidating } = useSWR<any>(
     `/api/weather?location=${location}&top=true`,
     fetcher
@@ -28,12 +46,12 @@ const Header = () => {
           jweath
         </h1>
       </Parallax>
-      <div className="my-6 w-[80vw] bg-black bg-opacity-70 px-2 text-center sm:w-[34rem]">
-        <Typical
-          steps={['get updated weather from places around the world🌏']}
-          loop={1}
-        />
-      </div>
+      <Parallax speed={5}>
+        <div className="my-6 w-[80vw] bg-black bg-opacity-70 px-2 text-center sm:w-[34rem]">
+          <TypingAnimation />
+        </div>
+      </Parallax>
+
       <Parallax speed={-2} scale={[1, 0.8]} translateY={[-10, 20]}>
         <div className="mb-7 w-[80vw] rounded-lg border px-5 py-2 shadow-2xl drop-shadow-2xl sm:w-[18rem]">
           {child}
